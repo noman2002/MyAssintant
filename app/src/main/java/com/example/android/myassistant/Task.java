9e4dfdf3beacc8275b06868b52d5f8e3fd99429b
iamnoman <nomn2002@gmail.com>
2019-11-18 11:24:27 +0530

    To Do app

---
+++ app/src/main/java/com/example/android/myassistant/Task.java
@@ -0,0 +1,35 @@
+package com.example.android.myassistant;
+
+public class Task {
+    String heading;
+    String date;
+
+    public Task(String heading, String date) {
+        this.heading = heading;
+        this.date = date;
+    }
+public Task(){
+
+}
+    public String getHeading() {
+
+        return heading;
+    }
+
+    public String getDate() {
+
+        return date;
+    }
+
+    public void setHeading(String heading) {
+
+        this.heading = heading;
+    }
+
+    public void setDate(String date) {
+
+        this.date = date;
+    }
+
+
+}
