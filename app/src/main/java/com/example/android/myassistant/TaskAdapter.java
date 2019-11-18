93ec3ad00558111b667c0d8341df16a28707579a
iamnoman <nomn2002@gmail.com>
2019-11-18 14:18:41 +0530

    First commit

--- app/src/main/java/com/example/android/myassistant/TaskAdapter.java
+++ app/src/main/java/com/example/android/myassistant/TaskAdapter.java
@@ -1,5 +1,6 @@
 package com.example.android.myassistant;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -7,22 +8,31 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder> {
+    ArrayList<Task> db;
+
+    public TaskAdapter(ArrayList<Task> db) {
+        this.db = db;
+    }
 
     @NonNull
     @Override
     public TaskViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new TaskViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent,false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull TaskViewHolder holder, int position) {
-
+     Task currentTask = db.get(position);
+     holder.heading.setText(currentTask.getHeading());
+     holder.date.setText(currentTask.getDate());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return db.size();
     }
 
     public class TaskViewHolder extends RecyclerView.ViewHolder{
